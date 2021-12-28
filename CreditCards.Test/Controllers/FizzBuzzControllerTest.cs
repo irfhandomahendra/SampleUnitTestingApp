25b835189aba7e82e298c8b52784a57b791ea31b
irfhandomahendra <irfhandomahendra@gmail.com>
2021-12-28 15:31:41 +0700

    contoh TDD

---
+++ CreditCards.Test/Controllers/FizzBuzzControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using SampleUnitTestingApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CreditCards.Test.Controllers
+{
+    public class FizzBuzzControllerTest
+    {
+        [Fact]
+        public void Given1Return1()
+        {
+            FizzBuzzController controller = new FizzBuzzController();
+            string expected = "1 ";
+            string actual = controller.Get(1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Given3Return12Fizz()
+        {
+            FizzBuzzController controller = new FizzBuzzController();
+            string expected = "1 2 Fizz ";
+            string actual = controller.Get(3);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Given5Return12Fizz4Buzz()
+        {
+            FizzBuzzController controller = new FizzBuzzController();
+            string expected = "1 2 Fizz 4 Buzz ";
+            string actual = controller.Get(5);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Given15ReturnFizzBuzz()
+        {
+            FizzBuzzController controller = new FizzBuzzController();
+            string expected = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz ";
+            string actual = controller.Get(15);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ SampleUnitTestingApp/Controllers/FizzBuzzController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleUnitTestingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FizzBuzzController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(int value)
+        {
+            string result = string.Empty;
+            for(int i =1; i<=value; i++)
+            {
+                if (i % 3 == 0 && i%5==0)
+                    result += $"FizzBuzz ";
+                else if (i % 3 == 0)
+                    result += $"Fizz ";
+                else if (i % 5 == 0)
+                    result += $"Buzz ";
+                else
+                    result += $"{i} ";
+            }
+            return result;
+        }
+    }
+}
